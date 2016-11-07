d87a8f916a241ff84f9fdad67d1812f58e0d3efb
root <root@6e017b01784a>
2016-11-07 12:39:39 +0000

    Initial Push

---
+++ services/cx2/src/com/civicxpress/cx2/service/Cx2ProcedureExecutorServiceImpl.java
@@ -0,0 +1,42 @@
+/*Copyright (c) 2015-2016 wavemaker-com All Rights Reserved.
+ This software is the confidential and proprietary information of wavemaker-com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with wavemaker-com*/
+
+package com.civicxpress.cx2.service;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.dao.procedure.WMProcedureExecutor;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+@Service
+public class Cx2ProcedureExecutorServiceImpl implements Cx2ProcedureExecutorService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Cx2ProcedureExecutorServiceImpl.class);
+
+	@Autowired
+	@Qualifier("cx2WMProcedureExecutor")
+	private WMProcedureExecutor procedureExecutor;
+
+
+	@Transactional(value = "cx2TransactionManager")
+	@Override
+	public List<Object> executeWMCustomProcedure(CustomProcedure procedure) {
+	    return procedureExecutor.executeCustomProcedure(procedure);
+	}
+
+
+}
+
---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "vermillion",
+  "defaultLanguage" : "en",
+  "displayName" : "CivicXpress",
+  "homePage" : "Main",
+  "name" : "CivicXpress",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
