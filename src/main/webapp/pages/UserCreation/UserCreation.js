9d478e182808f373e232d7073e9d78b43dac1871
sagar vemala <sagar.vemala@wavemaker.com>
2016-11-10 06:33:28 +0000

    New files added - [] Files modified - [UserCreation.js,ManageGISData.variables.json,app.css,header.html,ManageGISData.html,UserCreation.variables.json,intercept-urls.json,project-security-provider.xml] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/UserCreation/UserCreation.js
+++ src/main/webapp/pages/UserCreation/UserCreation.js
@@ -1,8 +1,9 @@
 Application.$controller("UserCreationPageController", ["$scope", function($scope) {
     "use strict";
     $scope.newUser;
-    /* perform any action on widgets/variables within this block */
+
     $scope.onPageReady = function() {
+        //Current Date for subscriptions
         $scope.toDay = Date.parse(new Date().toDateString());
 
     };
@@ -14,6 +15,7 @@ Application.$controller("UserCreationPageController", ["$scope", function($scope
     };
 
     $scope.CreateUseronSuccess = function(variable, data) {
+        // Saving newUser Object for Further trancastions(Subscribing & Roles)
         $scope.newUser = data;
         $scope.newUserId = data.id;
 
@@ -22,6 +24,7 @@ Application.$controller("UserCreationPageController", ["$scope", function($scope
 
     $scope.wizard1Done = function($isolateScope, steps) {
         for (var i = 0; i < selectedMunicipalites.length; i++) {
+            // For Registering User  for subscribed municialities        
             $scope.Variables.RegisterSubscriptions.setInput({
                 "dateSubscribed": $scope.toDay,
                 "users": $scope.newUser,
@@ -30,6 +33,19 @@ Application.$controller("UserCreationPageController", ["$scope", function($scope
                 "municipalityId": selectedMunicipalites[i].id
             });
             $scope.Variables.RegisterSubscriptions.insertRecord();
+
+            // For Registering User with Role as USER for subscribed municialities
+            $scope.Variables.NewUserRole.setInput({
+                "roleName": "User",
+                "description": "User",
+                "users": $scope.newUser,
+                "userId": $scope.newUser.id,
+                "municipalities": selectedMunicipalites[i],
+                "municipalityId": selectedMunicipalites[i].id
+            });
+            $scope.Variables.NewUserRole.insertRecord();
+
+
         }
         $scope.Variables.NewUserToLogin.navigate();
 
