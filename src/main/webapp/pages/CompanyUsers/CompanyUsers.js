b2bbf4858a84d5f5870c06c331dac4ae5706c8aa
sagar vemala <sagar.vemala@wavemaker.com>
2016-12-06 09:11:40 +0000

    New files added - [] Files modified - [CompanyUsers.js,CompanyUsers.variables.json,CompanyUsers.html] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/CompanyUsers/CompanyUsers.js
+++ src/main/webapp/pages/CompanyUsers/CompanyUsers.js
@@ -1,25 +1,24 @@
-Application.$controller("CompanyUsersPageController", ["$scope", function ($scope) {
+Application.$controller("CompanyUsersPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
-        /*
-         * variables can be accessed through '$scope.Variables' property here
-         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
-         * $scope.Variables.loggedInUser.getData()
-         *
-         * widgets can be accessed through '$scope.Widgets' property here
-         * e.g. to get value of text widget named 'username' use following script
-         * '$scope.Widgets.username.datavalue'
-         */
+    $scope.onPageReady = function() {
+        //current date
+        $scope.toDay = Date.parse(new Date().toDateString());
     };
 
 }]);
 
-
 Application.$controller("gridVendorUsersController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
+
+Application.$controller("dialogAddUserController", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
