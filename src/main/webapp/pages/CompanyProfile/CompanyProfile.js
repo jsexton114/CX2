e8644295a78420ce5370c3ffd1c9673a22283054
sagar vemala <sagar.vemala@wavemaker.com>
2016-12-07 16:15:32 +0000

    New files added - [] Files modified - [MunicipalitySettings.html,MunicipalitySettings.variables.json,CompanyProfile.html,CompanyProfile.js,VendorApplication.html,Main.js,VendorApplication.variables.json,CompanyProfile.variables.json] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/CompanyProfile/CompanyProfile.js
+++ src/main/webapp/pages/CompanyProfile/CompanyProfile.js
@@ -3,15 +3,9 @@ Application.$controller("CompanyProfilePageController", ["$scope", function($sco
 
     /* perform any action on widgets/variables within this block */
     $scope.onPageReady = function() {
-        /*
-         * variables can be accessed through '$scope.Variables' property here
-         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
-         * $scope.Variables.loggedInUser.getData()
-         *
-         * widgets can be accessed through '$scope.Widgets' property here
-         * e.g. to get value of text widget named 'username' use following script
-         * '$scope.Widgets.username.datavalue'
-         */
+        //current date
+        $scope.Variables.Today.dataSet.dataValue = Date.parse(new Date().toDateString());
+
     };
 
 
