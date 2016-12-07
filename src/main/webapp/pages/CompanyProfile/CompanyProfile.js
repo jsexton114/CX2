c0039dd1542793d8b0fd1fc2cb5d8b13a27e9d9a
Anvesh Nara <anvesh.nara@wavemaker.com>
2016-12-07 11:25:03 +0000

    New files added - [] Files modified - [CompanyProfile.html,ManageUsers.html,CompanyProfile.js,CompanyProfile.variables.json] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/CompanyProfile/CompanyProfile.js
+++ src/main/webapp/pages/CompanyProfile/CompanyProfile.js
@@ -1,8 +1,8 @@
-Application.$controller("CompanyProfilePageController", ["$scope", function ($scope) {
+Application.$controller("CompanyProfilePageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -14,12 +14,15 @@ Application.$controller("CompanyProfilePageController", ["$scope", function ($sc
          */
     };
 
+
+
+
 }]);
 
 
 Application.$controller("liveform1Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
