3b408aa5e389be0d439b0fa7f424020572284c7f
Anvesh Nara <anvesh.nara@wavemaker.com>
2016-12-15 08:39:15 +0000

    New files added - [] Files modified - [MunicipalityUsers.variables.json,MunicipalityUsers.html,Login.variables.json,Login.js] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/Login/Login.js
+++ src/main/webapp/pages/Login/Login.js
@@ -1,24 +1,38 @@
-Application.$controller("LoginPageController", ["$scope",
-    function ($scope) {
+Application.$controller("LoginPageController", ["$scope", "wmToaster",
+    function($scope, wmToaster) {
         "use strict";
 
         /* perform any action on widgets/variables within this block */
-        $scope.onPageReady = function () {
+        $scope.onPageReady = function() {
             /*
              * variables can be accessed through '$scope.Variables' property here
              * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
              * $scope.Variables.loggedInUser.getData()
-             * 
+             *
              * widgets can be accessed through '$scope.Widgets' property here
              * e.g. to get value of text widget named 'username' use following script
              * '$scope.Widgets.username.datavalue'
              */
+
         };
+
+        $scope.loginVariableonSuccess = function(variable, data) {
+            $scope.Variables.userDetails.setFilter('email', $("input[name='usernametext'").val());
+            $scope.Variables.userDetails.update({}, function(data) {
+                if (data[0].banned == true) {
+                    $scope.Variables.logoutVariable.invoke({}, function(data) {
+                        wmToaster.show('error', 'ERROR', 'Your account has been banned. If you have any questions, please contact support at 614-737-3743.', 5000);
+
+                    });
+                }
+            })
+        };
+
     }
 ]);
 
 Application.$controller("loginFormController", ["$scope",
-    function ($scope) {
+    function($scope) {
         "use strict";
         $scope.ctrlScope = $scope;
     }
