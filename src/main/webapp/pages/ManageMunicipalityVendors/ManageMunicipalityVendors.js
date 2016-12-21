000afee579015b164e1d46551ebdafc90e7ea5b9
Anvesh Nara <anvesh.nara@wavemaker.com>
2016-12-21 13:30:56 +0000

    ManageMunicipalityVendors Profile for all status Files modified - [ManageMunicipalityVendors.js,ManageMunicipalityVendors.variables.json,ManageMunicipalityVendors.html]

--- src/main/webapp/pages/ManageMunicipalityVendors/ManageMunicipalityVendors.js
+++ src/main/webapp/pages/ManageMunicipalityVendors/ManageMunicipalityVendors.js
@@ -1,8 +1,8 @@
-Application.$controller("ManageMunicipalityVendorsPageController", ["$scope", function ($scope) {
+Application.$controller("ManageMunicipalityVendorsPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -14,84 +14,116 @@ Application.$controller("ManageMunicipalityVendorsPageController", ["$scope", fu
          */
     };
 
+
+
+
 }]);
 
-
 Application.$controller("gridPendingController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+
+        $scope.customRowAction = function($event, $rowData) {
+            $scope.Variables.VendorProfile.setFilter('id', $rowData.vendorId)
+            $scope.Variables.VendorProfile.update();
+            $scope.Variables.VendorLicensesData.setFilter('id', $rowData.vendorId)
+            $scope.Variables.VendorLicensesData.update();
+            $scope.Widgets.viewCompanyDetails.open();
+
+
+
+        };
+
+
+
+
+    }
 ]);
 
 Application.$controller("liveform2Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("gridApprovedVendorsController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+
+        $scope.customRowAction = function($event, $rowData) {
+            $scope.Variables.VendorProfile.setFilter('id', $rowData.vendorId)
+            $scope.Variables.VendorProfile.update();
+            $scope.Variables.VendorLicensesData.setFilter('id', $rowData.vendorId)
+            $scope.Variables.VendorLicensesData.update();
+            $scope.Widgets.viewCompanyDetails.open();
+
+
+
+        };
+
+    }
 ]);
 
 Application.$controller("liveformApprovedVendorsController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("gridSuspendedController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+
+        $scope.customRowAction = function($event, $rowData) {
+            $scope.Variables.VendorProfile.setFilter('id', $rowData.vendorId)
+            $scope.Variables.VendorProfile.update();
+            $scope.Variables.VendorLicensesData.setFilter('id', $rowData.vendorId)
+            $scope.Variables.VendorLicensesData.update();
+            $scope.Widgets.viewCompanyDetails.open();
+
+
+
+        };
+
+    }
 ]);
 
 Application.$controller("liveformSuspendedController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("viewCompanyDetailsController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
-Application.$controller("liveform1Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+Application.$controller("grid5Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("grid4Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
-]);
-
-
-
-Application.$controller("grid5Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("grid6Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
