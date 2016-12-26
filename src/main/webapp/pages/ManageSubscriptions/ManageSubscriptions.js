682eaf5b74cc668c7adbddcd43582f1e1bc33f0c
sagar vemala <sagar.vemala@wavemaker.com>
2016-12-26 08:35:39 +0000

    CXAdmin - ManageSubscriptions default date(read-only) Files modified - [ManageSubscriptions.js,EditForm.html,ManageSubscriptions.html,app.variables.json]

--- src/main/webapp/pages/ManageSubscriptions/ManageSubscriptions.js
+++ src/main/webapp/pages/ManageSubscriptions/ManageSubscriptions.js
@@ -3,15 +3,7 @@ Application.$controller("ManageSubscriptionsPageController", ["$scope", function
 
     /* perform any action on widgets/variables within this block */
     $scope.onPageReady = function() {
-        $scope.Variables.BreadCrum.dataSet = [{
-            "label": "Home",
-            "icon": "glyphicon glyphicon-home",
-            "link": "#/CXAdmin"
-        }, {
-            "label": "ManageSubscriptions",
-            "icon": "wi wi-ellipsis-v",
-            "link": "#/ManageSubscriptions"
-        }]
+
     };
 
 }]);
@@ -22,15 +14,15 @@ Application.$controller("ManageSubscriptionsPageController", ["$scope", function
 
 
 Application.$controller("grid2Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("liveform2Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
