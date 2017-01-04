54197f288c2de799c84be196aed080ff5b28cdec
Jason Keller <jkeller@tekdoginc.com>
2017-01-04 21:12:23 +0000

    re-added postal code Files modified - [cx2_published_dataModel.json,servicedef.xml,Gisrecords.java]

--- services/cx2/src/com/civicxpress/cx2/Gisrecords.java
+++ services/cx2/src/com/civicxpress/cx2/Gisrecords.java
@@ -50,6 +50,7 @@ public class Gisrecords implements Serializable {
     private String longitude;
     private String country;
     private String unitNumber;
+    private String postalCode;
     private Municipalities municipalities;
     private States states;
     private Subdivisions subdivisions;
@@ -218,6 +219,15 @@ public class Gisrecords implements Serializable {
 
     public void setUnitNumber(String unitNumber) {
         this.unitNumber = unitNumber;
+    }
+
+    @Column(name = "`PostalCode`", nullable = true, length = 255)
+    public String getPostalCode() {
+        return this.postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 
     @ManyToOne(fetch = FetchType.EAGER)
