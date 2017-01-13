5605304820da496835f43d8cefb2bfb630f20e39
sagar vemala <sagar.vemala@wavemaker.com>
2017-01-13 07:52:45 +0000

    GIS Maintenance- MuniEmployee can't see GIS Record panel Disable according to role Files modified - [EditGISRecord.js,pages-config.json,EditGISRecord.html]

--- src/main/webapp/pages/EditGISRecord/EditGISRecord.js
+++ src/main/webapp/pages/EditGISRecord/EditGISRecord.js
@@ -1,8 +1,8 @@
-Application.$controller("EditGISRecordPageController", ["$scope", function ($scope) {
+Application.$controller("EditGISRecordPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -17,23 +17,28 @@ Application.$controller("EditGISRecordPageController", ["$scope", function ($sco
 }]);
 
 
-Application.$controller("liveformGisrecordsController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+Application.$controller("liveformGisrecordsController", ["$scope", "$rootScope",
+    function($scope, $rs) {
+        "use strict";
+
+        if ($rs.Variables.loggedInUser.dataSet.roles[0] == "MunicipalityEmployee")
+            $scope.disable = true;
+        else
+            $scope.disable = false;
+    }
 ]);
 
+
 Application.$controller("gridGISContactsController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("liveformGISContactsController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
