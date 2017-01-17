f3f1872128b09fc166f848fe286df5b0924d03dc
sagar vemala <sagar.vemala@wavemaker.com>
2017-01-17 09:31:17 +0000

    Form Status Wizard Visualization Files modified - [Forms.variables.json,Forms.html,Forms.js]

--- src/main/webapp/pages/Forms/Forms.js
+++ src/main/webapp/pages/Forms/Forms.js
@@ -1,4 +1,4 @@
-Application.$controller("FormsPageController", ["$scope", function($scope) {
+Application.$controller("FormsPageController", ["$scope", "$timeout", function($scope, $timeout) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
@@ -6,25 +6,36 @@ Application.$controller("FormsPageController", ["$scope", function($scope) {
         $scope.today = Date.parse(new Date().toDateString());
     };
 
+
+    $scope.FormStatusonSuccess = function(variable, data) {
+        // For showing current Status of form
+        var currentStatus = _.findIndex(data, {
+            'id': _.parseInt($scope.pageParams.FormStatusId)
+        });
+        $timeout(function() {
+            var a = $('.livelist-status li.app-list-item:nth-child(' + (currentStatus + 1) + ')').addClass('active');
+        });
+    };
+
 }]);
 
 Application.$controller("gridFormhistoryController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("gridSharedwithController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("liveformSharedwithController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
