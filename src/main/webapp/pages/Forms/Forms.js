420c3e7a66c2d6d4e81a13717fc53f72db7ec8cd
sagar vemala <sagar.vemala@wavemaker.com>
2017-01-18 06:35:00 +0000

    - Recording time for History by moment - Delete for Conatcts Files modified - [MunicipalitySettings.variables.json,Forms.variables.json,Forms.html,Forms.js]

--- src/main/webapp/pages/Forms/Forms.js
+++ src/main/webapp/pages/Forms/Forms.js
@@ -3,7 +3,9 @@ Application.$controller("FormsPageController", ["$scope", "$timeout", function($
 
     /* perform any action on widgets/variables within this block */
     $scope.onPageReady = function() {
-        $scope.today = Date.parse(new Date().toDateString());
+        //$scope.today = Date.parse(new Date().toDateString());
+        $scope.today = moment().valueOf();
+
     };
 
 
@@ -12,6 +14,7 @@ Application.$controller("FormsPageController", ["$scope", "$timeout", function($
         var currentStatus = _.findIndex(data, {
             'id': _.parseInt($scope.pageParams.FormStatusId)
         });
+        $scope.defaultObjectForSelectStatus = data[currentStatus];
         $timeout(function() {
             var a = $('.livelist-status li.app-list-item:nth-child(' + (currentStatus + 1) + ')').addClass('active');
         });
