e95238231a6bf4837e5726ddaf656ccc0df1db10
Emma Carpenter <emma@tekdoginc.com>
2017-01-25 15:59:55 +0000

    Finalized the Form naming options section on EditForm Files modified - [pages-config.json,EditForm.variables.json,EditForm.js,app.css,EditForm.html]

--- src/main/webapp/pages/EditForm/EditForm.js
+++ src/main/webapp/pages/EditForm/EditForm.js
@@ -6,9 +6,6 @@ Application.$controller("EditFormPageController", ["$scope", "wmToaster", functi
 
     };
 
-
-
-
     $scope.liveform2Error = function($event, $operation, $data) {
         wmToaster.show('error', 'ERROR', 'Sort Order cannot be duplicated.', 5000);
     };
@@ -20,6 +17,48 @@ Application.$controller("liveformUpdateFormTypeController", ["$scope",
     function($scope) {
         "use strict";
         $scope.ctrlScope = $scope;
+
+        $scope.prefixDateChange = function($event, $isolateScope, newVal, oldVal) {
+            if (newVal === 'None') {
+                $scope.Widgets.liveformUpdateFormType.formWidgets.prefixNumber.datavalue = 'AutoIncrement';
+            }
+        };
+
+        $scope.generateFormTitlePreview = function() {
+            var prefix = $scope.Widgets.liveformUpdateFormType.formWidgets.titlePrefix.datavalue;
+            var dateOption = $scope.Widgets.liveformUpdateFormType.formWidgets.prefixDate.datavalue;
+            var numberOption = $scope.Widgets.liveformUpdateFormType.formWidgets.prefixNumber.datavalue;
+            var prefixDashes = $scope.Widgets.liveformUpdateFormType.formWidgets.prefixDashes.datavalue;
+            var numberStart = $scope.Widgets.liveformUpdateFormType.formWidgets.prefixNumberStart.datavalue;
+
+            var formTitlePreview = '' + prefix;
+
+            var today = new Date();
+
+            if (dateOption !== 'None') {
+                var year = today.getFullYear();
+                var month = today.getMonth() + 1;
+
+                month = (month < 10) ? '0' + month : month;
+                if (prefixDashes === true) {
+                    formTitlePreview += '-';
+                }
+                if (dateOption === 'MonthYear') {
+                    formTitlePreview += month + '' + year;
+                } else {
+                    formTitlePreview += year + '' + month;
+                }
+            }
+
+            if (prefixDashes === true) {
+                formTitlePreview += '-';
+            }
+
+            formTitlePreview += numberStart;
+
+            return formTitlePreview;
+        };
+
     }
 ]);
 
