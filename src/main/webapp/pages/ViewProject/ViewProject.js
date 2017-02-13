551a82a5121955ed40503c53d6b296bcec61e043
sagar vemala <sagar.vemala@wavemaker.com>
2017-02-13 10:21:34 +0000

    Add member Availablity to CreatedBy and (MunicipalityAdmin or CXAdmin) Files modified - [ViewProject.js,ViewProject.html,ViewProject.variables.json]

--- src/main/webapp/pages/ViewProject/ViewProject.js
+++ src/main/webapp/pages/ViewProject/ViewProject.js
@@ -3,9 +3,26 @@ Application.$controller("ViewProjectPageController", ["$scope", function($scope)
 
     /* perform any action on widgets/variables within this block */
     var crum;
+    $scope.addMember;
     $scope.onPageReady = function() {
         $scope.today = moment().valueOf();
         //var temp = $scope.Variables.BreadCrumb.dataSet;
+        $scope.addMember = false;
+    };
+
+
+    $scope.CurrentProjectonSuccess = function(variable, data) {
+        debugger
+        if ($scope.Variables.loggedInUser.dataSet.id == data[0].usersByCreatedBy.id) {
+            $scope.addMember = true;
+        }
+        var temp = $scope.Variables.loggedInUser.dataSet.roles;
+        //Checking if user is muniadmin or cxadmin
+        for (let i = 0; i < temp.length; i++) {
+            if ((temp[i] == "MunicipalityAdmin") || (temp[i] == "CXAdmin")) {
+                $scope.addMember = true;
+            }
+        }
     };
 
 }]);
