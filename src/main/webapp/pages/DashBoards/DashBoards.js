7f00e904c7c52bbb9dd75dedaff9a574ded7279b
sagar vemala <sagar.vemala@wavemaker.com>
2017-03-13 12:50:03 +0000

    Layout and bug fix for Dashboard Files modified - [DashBoards.js,DashBoards.html]

--- src/main/webapp/pages/DashBoards/DashBoards.js
+++ src/main/webapp/pages/DashBoards/DashBoards.js
@@ -3,15 +3,9 @@ Application.$controller("DashBoardsPageController", ["$scope", function($scope) 
 
     /* perform any action on widgets/variables within this block */
     $scope.onPageReady = function() {
-        /*
-         * variables can be accessed through '$scope.Variables' property here
-         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
-         * $scope.Variables.loggedInUser.getData()
-         *
-         * widgets can be accessed through '$scope.Widgets' property here
-         * e.g. to get value of text widget named 'username' use following script
-         * '$scope.Widgets.username.datavalue'
-         */
+        //setting default to year
+        $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+        $scope.Widgets.datetimeStart.timestamp = moment().subtract(1, 'years').valueOf();
     };
 
 
