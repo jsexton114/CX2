2f3f528c5549fd02ae832ba559b5c2ab6e68ce80
sagar vemala <sagar.vemala@wavemaker.com>
2017-03-13 12:24:11 +0000

    Logic to invoke queries at time change Files modified - [DashBoards.js,DashBoards.variables.json,DashBoards.html]

--- src/main/webapp/pages/DashBoards/DashBoards.js
+++ src/main/webapp/pages/DashBoards/DashBoards.js
@@ -1,8 +1,8 @@
-Application.$controller("DashBoardsPageController", ["$scope", function ($scope) {
+Application.$controller("DashBoardsPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -12,6 +12,38 @@ Application.$controller("DashBoardsPageController", ["$scope", function ($scope)
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
          */
+    };
+
+
+    $scope.selectTermsChange = function($event, $isolateScope, newVal, oldVal) {
+        switch (newVal) {
+
+            case "Year":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(1, 'years').valueOf();
+                break;
+            case "Half":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(6, 'months').valueOf();
+                break;
+            case "Quarter":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(4, 'months').valueOf();
+                break;
+            case "Month":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(1, 'months').valueOf();
+                break;
+            case "Week":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(1, 'weeks').valueOf();
+                break;
+            case "Today":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().startOf('day').valueOf();
+                break;
+
+        }
     };
 
 }]);
