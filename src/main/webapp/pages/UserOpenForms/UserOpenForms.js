26cedc56cb04990950f3a2c50f60ef5194bb6328
Emma Carpenter <emma@tekdoginc.com>
2017-03-30 21:13:05 +0000

    Drafts tab on UserOpenForms Files modified - [UserOpenForms.js,UserOpenForms.variables.json,UserOpenForms.html]

--- src/main/webapp/pages/UserOpenForms/UserOpenForms.js
+++ src/main/webapp/pages/UserOpenForms/UserOpenForms.js
@@ -1,8 +1,8 @@
-Application.$controller("UserOpenFormsPageController", ["$scope", function ($scope) {
+Application.$controller("UserOpenFormsPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -20,36 +20,43 @@ Application.$controller("UserOpenFormsPageController", ["$scope", function ($sco
 
 
 Application.$controller("dialogCompaniesListController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("gridCompaniesListController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("gridOpenFormsController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("dialogNewFormController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
-Application.$controller("grid2Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+Application.$controller("grid2Controller", ["$scope", "$location",
+    function($scope, $location) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+
+        $scope.updaterowAction = function($event, $rowData) {
+            $location.path("/NewForm").search({
+                draftId: $rowData.id
+            });
+        };
+
+    }
 ]);
