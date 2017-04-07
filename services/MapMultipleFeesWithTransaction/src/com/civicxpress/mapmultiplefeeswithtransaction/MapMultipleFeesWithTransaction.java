9dfb10ed2594207ef93d154cefc5ad1f949dde33
sagar vemala <sagar.vemala@wavemaker.com>
2017-04-07 11:06:19 +0000

    Java Service to MultipleInsert Transaction and  Fees New files added - [MapMultipleFeesWithTransaction] Files modified - [MunicipalityCheckOut.js,project-services.xml,pom.xml,MunicipalityCheckOut.variables.json]

---
+++ services/MapMultipleFeesWithTransaction/src/com/civicxpress/mapmultiplefeeswithtransaction/MapMultipleFeesWithTransaction.java
@@ -0,0 +1,91 @@
+/*Copyright (c) 2015-2016 wavemaker-com All Rights Reserved.
+ This software is the confidential and proprietary information of wavemaker-com You shall not disclose such Confidential Information and shall use it only in accordance
+ with the terms of the source code license agreement you entered into with wavemaker-com*/
+package com.civicxpress.mapmultiplefeeswithtransaction;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import com.wavemaker.runtime.security.SecurityService;
+import com.wavemaker.runtime.service.annotations.ExposeToClient;
+import com.wavemaker.runtime.service.annotations.HideFromClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import org.apache.commons.io.IOUtils;
+import com.fasterxml.jackson.databind.*;
+
+import com.civicxpress.cx2.TransactionToFees;
+import com.civicxpress.cx2.service.TransactionToFeesService;
+
+//import com.civicxpress.mapmultiplefeeswithtransaction.model.*;
+
+/**
+ * This is a singleton class with all its public methods exposed as REST APIs via generated controller class.
+ * To avoid exposing an API for a particular public method, annotate it with @HideFromClient.
+ *
+ * Method names will play a major role in defining the Http Method for the generated APIs. For example, a method name
+ * that starts with delete/remove, will make the API exposed as Http Method "DELETE".
+ *
+ * Method Parameters of type primitives (including java.lang.String) will be exposed as Query Parameters &
+ * Complex Types/Objects will become part of the Request body in the generated API.
+ */
+@ExposeToClient
+public class MapMultipleFeesWithTransaction {
+
+    private static final Logger logger = LoggerFactory.getLogger(MapMultipleFeesWithTransaction.class);
+
+    @Autowired
+    private SecurityService securityService;
+    
+     @Autowired
+    private TransactionToFeesService transactionToFeesService;
+
+     
+    ObjectMapper objectMapper = new ObjectMapper();
+    
+    public void tagFeeWithTransaction(final String feeListString) {
+     ObjectMapper objectMapper = new ObjectMapper();
+     try {
+        Map map = objectMapper.readValue(feeListString, Map.class);
+        logger.info(feeListString);
+        List list = (List) map.get("content");
+        for(int i=0;i<list.size();i++){
+            createTable((Map)list.get(i));
+        }
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+    
+}
+
+
+   // method to insert data into table
+public TransactionToFees createTable(Map feeItems)
+    { 
+        TransactionToFees transactionToFees = new TransactionToFees();
+        transactionToFees.setFeeId((Integer)feeItems.get("feeId"));
+        transactionToFees.setTransactionId((Integer)feeItems.get("transactionId"));
+        TransactionToFees persistedTable = transactionToFeesService.create(transactionToFees);
+        return persistedTable;
+    }
+
+}
--- src/main/webapp/pages/MunicipalityCheckOut/MunicipalityCheckOut.js
+++ src/main/webapp/pages/MunicipalityCheckOut/MunicipalityCheckOut.js
@@ -23,14 +23,36 @@ Application.$controller("MunicipalityCheckOutPageController", ["$scope", functio
         for (let i = 0; i < temp.length; i++) {
             fees.push(temp[i].feeId);
         }
+
+        //Updating the Status of fees to Paid after Payment
         $scope.Variables.svUpdateMultipleFeeStatus.setInput({
             'feeList': fees
         });
         $scope.Variables.svUpdateMultipleFeeStatus.update();
+        // Removing the fee from All users Carts
         $scope.Variables.svRemoveFeesFromAllUserCarts.setInput({
             'feeList': fees
         });
         $scope.Variables.svRemoveFeesFromAllUserCarts.update();
+
+        let feeData = $scope.Variables.svCartItemIds.dataSet.content;
+        let targetList = []
+        _.forEach(feeData, function(obj) {
+            targetList.push({
+                'feeId': obj.feeId,
+                'transactionId': data.transactionId
+            });
+        });
+
+        let itemSet = {
+            'content': targetList
+        };
+        let mappedList = JSON.stringify(itemSet);
+        $scope.Variables.svMapFeeWithTransaction.setInput({
+            'feeListString': mappedList
+        });
+        $scope.Variables.svMapFeeWithTransaction.update();
+
     };
 
 }]);
