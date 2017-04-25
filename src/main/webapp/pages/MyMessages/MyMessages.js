abea3b7ba6def8aac5258736a5225fec8c5d5fa9
Sagar Vemala <sagar.vemala@wavemaker.com>
2017-04-25 11:56:42 +0000

    tweaks for the reply message Files modified - [MyMessages]

--- src/main/webapp/pages/MyMessages/MyMessages.js
+++ src/main/webapp/pages/MyMessages/MyMessages.js
@@ -3,15 +3,7 @@ Application.$controller("MyMessagesPageController", ["$scope", function($scope) 
 
     /* perform any action on widgets/variables within this block */
     $scope.onPageReady = function() {
-        /*
-         * variables can be accessed through '$scope.Variables' property here
-         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
-         * $scope.Variables.loggedInUser.getData()
-         *
-         * widgets can be accessed through '$scope.Widgets' property here
-         * e.g. to get value of text widget named 'username' use following script
-         * '$scope.Widgets.username.datavalue'
-         */
+
     };
 
 
