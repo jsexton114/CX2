d2d64babfaa46946abe589466ab1cf5a3698bd06
Jason Keller <jkeller@tekdoginc.com>
2017-05-09 21:01:38 +0000

    View Paid/Unpaid Fees PDF on transactions page Files modified - [MunicipalityTransactions,pom.xml]

--- src/main/webapp/pages/MunicipalityTransactions/MunicipalityTransactions.js
+++ src/main/webapp/pages/MunicipalityTransactions/MunicipalityTransactions.js
@@ -47,6 +47,23 @@ Application.$controller("MunicipalityTransactionsPageController", ["$scope", fun
     $scope.getEndDateTime = function() {
         return !!$scope.Widgets.datetimeEnd.datavalue ? moment($scope.Widgets.datetimeEnd.datavalue).endOf('day').valueOf() : undefined;
     };
+
+    $scope.btnViewReportClick = function($event, $isolateScope) {
+        var municipality = $scope.Widgets.selectMunicipality.datavalue.MunicipalityName;
+        var startDate = $scope.getFormattedDateForUrl($scope.Widgets.datetimeStart.datavalue);
+        var endDate = $scope.getFormattedDateForUrl($scope.Widgets.datetimeEnd.datavalue);
+        var viewReportUrl = 'http://67.52.205.86:8888/jinfonet/runReport.jsp?jrs.cmd=jrs.web_vw&jrs.report=%2fUSERFOLDERPATH%2fadmin%2fPaidUnpaidFeesByMunicipality.cls&jrs.catalog=%2fUSERFOLDERPATH%2fadmin%2fcx2Fallfield.cat&jrs.result_type=2&jrs.param$MunicipalityInput=' + municipality + '&jrs.param$StartDateInput=' + startDate + '&jrs.param$EndDateInput=' + endDate + '&jrs.auth_uid=admin&jrs.auth_pwd=admin';
+        console.log('municipality: ' + municipality);
+        console.log('viewReportUrl: ' + viewReportUrl);
+        window.open(viewReportUrl);
+    };
+
+    $scope.getFormattedDateForUrl = function(dateString) {
+        var d = new Date(dateString)
+        var formattedDate = d.toLocaleDateString('en-US');
+        return formattedDate;
+    }
+
 }]);
 
 Application.$controller("gridTransactionsController", ["$scope",
