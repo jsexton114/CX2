6d34c0364056732ef1b7742198f2e9b5209b35ba
Derek Smith <derek@tekdoginc.com>
2017-05-11 15:58:59 +0000

    MunicipalityForms-- built page basics, added a stock livegrid that will need to be replaced. "Select Date Range" is only visible to CXAdmin for now since it doesn't work, same with empty panel that will eventually have charts.
    MyMunicipality-- added MunicipalityForms tile with tile count not yet set.
    ProcessForms-- updated breadcrumb icon to match dashboard
    New files added - [MunicipalityForms]
    Files modified - [MyMunicipality,ProcessForms,app.variables.json,pages-config.json]

---
+++ src/main/webapp/pages/MunicipalityForms/MunicipalityForms.js
@@ -0,0 +1,70 @@
+Application.$controller("MunicipalityFormsPageController", ["$scope", function($scope) {
+    "use strict";
+
+    /* perform any action on widgets/variables within this block */
+    $scope.onPageReady = function() {
+        $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+        $scope.Widgets.datetimeStart.timestamp = moment.utc(new Date(2016, 6, 1)).valueOf();
+    };
+
+    $scope.selectTermsChange = function($event, $isolateScope, newVal, oldVal) {
+        switch (newVal) {
+            case "All Time":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment.utc(new Date(2016, 6, 1)).valueOf();
+                break;
+            case "This Year":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(1, 'years').valueOf();
+                break;
+            case "This Half":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(6, 'months').valueOf();
+                break;
+            case "This Quarter":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(4, 'months').valueOf();
+                break;
+            case "This Month":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(1, 'months').valueOf();
+                break;
+            case "This Week":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().subtract(1, 'weeks').valueOf();
+                break;
+            case "Today":
+                $scope.Widgets.datetimeEnd.timestamp = moment().valueOf();
+                $scope.Widgets.datetimeStart.timestamp = moment().startOf('day').valueOf();
+                break;
+        }
+    };
+
+    $scope.getStartDateTime = function() {
+        return !!$scope.Widgets.datetimeStart.datavalue ? moment($scope.Widgets.datetimeStart.datavalue).startOf('day').valueOf() : undefined;
+    };
+
+    $scope.getEndDateTime = function() {
+        return !!$scope.Widgets.datetimeEnd.datavalue ? moment($scope.Widgets.datetimeEnd.datavalue).endOf('day').valueOf() : undefined;
+    };
+
+}]);
+
+Application.$controller("gridMunicipalityFormsController", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+
+        $scope.customRowAction = function($event, $rowData) {
+            $scope.Widgets.pagedialogViewForm.open();
+
+        };
+    }
+]);
+
+Application.$controller("pagedialogViewFormController", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
